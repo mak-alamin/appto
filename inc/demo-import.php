1992373baa6240e61218ad826b2f5180c7b145f8
Mak Alamin <makalamin012@gmail.com>
2022-10-07 17:45:46 +0600

    fixed demo import issues

--- inc/demo-import.php
+++ inc/demo-import.php
@@ -1,7 +1,18 @@
 <?php
 
+function appto_custom_mime_types($mimes)
+{
+  $mimes['xml'] = 'application/xml';
+  $mimes['svg'] = 'image/svg+xml';
+  $mimes['svgz'] = 'image/svg+xml';
+
+  return $mimes;
+}
+add_filter('upload_mimes', 'appto_custom_mime_types');
+
 //Required and Recommended Plugins
-function appto_register_plugins( $plugins ) {
+function appto_register_plugins($plugins)
+{
   $theme_plugins = [
     [
       'name'     => 'Elementor',
@@ -15,19 +26,20 @@ function appto_register_plugins( $plugins ) {
       'required' => true,
     ],
   ];
- 
-  return array_merge( $plugins, $theme_plugins );
+
+  return $theme_plugins;
 }
-add_filter( 'ocdi/register_plugins', 'appto_register_plugins' );
+add_filter('ocdi/register_plugins', 'appto_register_plugins');
 
 
 //Modify plugin page attributes
-function appto_plugin_page_setup( $default_settings ) {
+function appto_plugin_page_setup($default_settings)
+{
   $default_settings['parent_slug'] = 'themes.php';
- 
-  $default_settings['page_title']  = esc_html__( 'APPTO Demo Import' , APPTO_TEXT_DOMAIN );
 
-  $default_settings['menu_title']  = esc_html__( 'APPTO Templates' , APPTO_TEXT_DOMAIN );
+  $default_settings['page_title']  = esc_html__('APPTO Demo Import', APPTO_TEXT_DOMAIN);
+
+  $default_settings['menu_title']  = esc_html__('APPTO Templates', APPTO_TEXT_DOMAIN);
 
   $default_settings['capability']  = 'import';
 
@@ -35,48 +47,73 @@ function appto_plugin_page_setup( $default_settings ) {
 
   return $default_settings;
 }
-add_filter( 'ocdi/plugin_page_setup', 'appto_plugin_page_setup' );
+add_filter('ocdi/plugin_page_setup', 'appto_plugin_page_setup');
 
 
 //Change OCDI plugin page intro text
-function appto_plugin_intro_text( $default_text ) {
+function appto_plugin_intro_text($default_text)
+{
   $default_text .= '<div class="ocdi__intro-text"><p>This is APP TO Demo Importer intro text.</p></div>';
 
   return $default_text;
 }
-add_filter( 'ocdi/plugin_intro_text', 'appto_plugin_intro_text' );
+add_filter('ocdi/plugin_intro_text', 'appto_plugin_intro_text');
 
 
 //Before Demo Import
-function appto_before_content_import( $selected_import ) {
-  if ( 'App To' === $selected_import['import_file_name'] ) {
-      echo "Before APP TO Demo Import Text...";
-  }
-  else {
-      // Here you can do stuff for all other imports before the content import starts.
-      echo "before import 2";
+function appto_before_content_import($selected_import)
+{
+  if ('App To' === $selected_import['import_file_name']) {
+    echo "Before APP TO Demo Import Text...";
+  } else {
+    // Here you can do stuff for all other imports before the content import starts.
+    echo "before import 2";
   }
 }
-add_action( 'ocdi/before_content_import', 'appto_before_content_import' );
+add_action('ocdi/before_content_import', 'appto_before_content_import');
 
 
 //Demo Import
-function appto_import_files() {
-    return [
-      [
-        'import_file_name'           => 'App To',
-        'categories'                 => [ 'Landing Page' ],
-        'import_file_url'            => get_template_directory_uri() . '/demo-data/appto_demo_data.xml',
-        'import_preview_image_url'   => APPTO_ASSETS . '/image/appto_demo_preview.png',
-        'preview_url'                => 'http://www.your_domain.com/my-demo-1',
-      ],
+function appto_import_files()
+{
+  return [
+    [
+      'import_file_name'           => 'App To',
+      'categories'                 => ['Landing Page'],
+      'import_file_url'            => get_template_directory_uri() . '/demo-data/appto_demo_data.xml',
+      'import_preview_image_url'   => APPTO_ASSETS . '/image/appto_demo_preview.png',
+      'preview_url'                => 'http://www.your_domain.com/appto/demo-1',
+    ],
 
-      [
-        'import_file_name'           => 'Comming Soon',
-        'categories'                 => [ 'Landing Page', 'E-Commerce', 'Blog' ],
-        'import_preview_image_url'   => APPTO_ASSETS . '/image/comming_soon.png',
-      ],
+    [
+      'import_file_name'           => 'Comming Soon',
+      'categories'                 => ['Landing Page', 'E-Commerce', 'Blog'],
+      'import_preview_image_url'   => APPTO_ASSETS . '/image/comming_soon.png',
+    ],
 
-    ];
-  }
-  add_filter( 'ocdi/import_files', 'appto_import_files' );
+  ];
+}
+add_filter('ocdi/import_files', 'appto_import_files');
+
+
+function appto_ocdi_after_import_setup()
+{
+  // Assign menus to their locations.
+  $main_menu = get_term_by('name', 'Main Menu', 'nav_menu');
+
+  set_theme_mod(
+    'nav_menu_locations',
+    [
+      'main-menu' => $main_menu->term_id, // replace 'main-menu' here with the menu location identifier from register_nav_menu() function in your theme.
+    ]
+  );
+
+  // Assign front page and posts page (blog page).
+  $front_page = get_page_by_title('Layout 1');
+  $blog_page  = get_page_by_title('Blog');
+
+  update_option('show_on_front', 'page');
+  update_option('page_on_front', $front_page->ID);
+  update_option('page_for_posts', $blog_page->ID);
+}
+add_action('ocdi/after_import', 'appto_ocdi_after_import_setup');
